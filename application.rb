126f25ab2ef4d29d4348cde7fed1171d976f8bc6
Christoph Helma <helma@in-silico.ch>
2010-08-09 10:03:11 +0200

    opentox-api-wrapper bumped to 1.6.1

--- application.rb
+++ application.rb
@@ -7,7 +7,7 @@ ENV["CLASSPATH"] = "#{ENV["CLASSPATH"]}:#{java_dir}:#{cdk}:#{jchempaint}"
 
 require 'rubygems'
 require 'rjb'
-gem "opentox-ruby-api-wrapper", "= 1.6.0"
+gem "opentox-ruby-api-wrapper", "= 1.6.1"
 require 'opentox-ruby-api-wrapper'
 
 #set :lock, true # avoid JVM memory allocation problems
